660f82fd6578ff12317140da06a1cbafed1a649a
Mouha-med-ouchen <mkayazmoha@gmail.com>
2024-10-27 14:39:40 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 let header = document.querySelector("header");
+let btnY = document.getElementById('btnY');
 
 window.onscroll = function() {
     if (this.scrollY >= 50) {
         header.classList.add('add-bac');
+
     } else {
-        header.classList.remove('add-bac')
+        header.classList.remove('add-bac');
+
     }
+
+
+
 }
 
 let nav_lenks = document.getElementById('links');
